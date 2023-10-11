191f794e1fc4980b88c702e9b99430db79dc1cfc
kdi-0 <kerwin.dahomey@gmail.com>
2023-10-11 00:01:57 -0500

    insert default hello world page for each route so git tracks them

---
+++ app/tos/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+                <div>Hello World from TOS and Agreement page</div>
+            </h2>
+        </main >
+    );
+}
