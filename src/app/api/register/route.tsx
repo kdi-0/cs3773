815ad12a1ace776927d2c924e336179a34d5d0e9
Ramin Mohammadi <rammoh5346@gmail.com>
2023-10-26 20:11:38 -0500

    Front end Cart page

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -6,8 +6,16 @@ export async function POST(request: Request) {
   const { name, email, password } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
-
+  
   // try {
   //   const user = await
   // }
+
+  /*
+  goals: check to see if the account already exists, 
+  if not, then add the account data to the database,
+  otherwise, output error message: Account already exists
+  */
+
+  
 }
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,93 @@
+import Navbar from "../../components/Navbar";
+
 export default async function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className={`mb-3 text-2xl font-semibold`}>
-        <div>Hello World from /cart</div>
-      </h2>
+    <main>
+      <div >
+        <Navbar/>
+      </div>
+      
+      <div className="container">
+        <div className="row">
+          <div className="d-flex align-items-start py-3 px-4 inline-block ">
+            <h2 className="text-2xl font-semibold">
+              Review Cart
+            </h2>
+            <hr style={{width: 300}}></hr>
+            <div>
+              <div className="text-left py-3 px-4 inline-block">
+                <img style={{width:50, height:100}} src="https://images.unsplash.com/photo-1470324161839-ce2bb6fa6bc3?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+              </div>
+              <div className="text-left py-3 px-4 inline-block">
+                <p className="font-semibold text-xs">Name of Item</p>
+                <p className="font-semibold text-xs">$45</p>
+                <p className="text-xs">Quantity: 3</p>
+              </div>
+              <div className="text-s">
+              <a className="px-4">Edit</a>
+              <a>Remove</a>
+              </div>
+            </div>
+
+            <hr style={{width: 200}}></hr>
+          </div>
+          
+          <div className="inline-block">
+          <div  className="text-right py-3 px-4 inline-block border p-3 d-flex justify-content-end">
+            <h3 className={` text-center text-xl font-semibold`}>
+              Order Summary
+            </h3>
+            <hr style={{width: 200}}></hr>
+            <div className="row">
+              <div className="text-left py-3 px-4 inline-block">
+                <p className="text-left">Item 1</p>
+              </div>
+              <div className="py-3 px-4 inline-block">
+                <p className="text-left">$345678</p>
+              </div>
+            </div>
+            <div className="row">
+               <div className="text-left py-3 px-4 inline-block">
+                <p className="text-left">Shipping</p>
+              </div>
+              <div className="py-3 px-4 inline-block">
+                <p className="text-left">$3.78</p>
+              </div>
+            </div>
+            <hr style={{width: 200}}></hr>
+            <div className="row">
+               <div className="text-left py-3 px-4 inline-block">
+                <p className="text-left">Subtotal</p>
+              </div>
+              <div className="py-3 px-4 inline-block">
+                <p className="text-left">$3333.78</p>
+              </div>
+            </div>
+            <div className="row">
+               <div className="text-left py-3 px-4 inline-block">
+                <p className="text-left">Taxes</p>
+              </div>
+              <div className="py-3 px-4 inline-block">
+                <p className="text-left">$32.78</p>
+              </div>
+            </div>
+            <hr style={{width: 200}}></hr>
+            <div className="row">
+               <div className="text-left py-3 px-4 inline-block">
+                <p className="font-semibold text-left">Order Total</p>
+              </div>
+              <div className="py-3 px-4 inline-block">
+                <p className="font-semibold text-left">$323.78</p>
+              </div>
+            </div>
+          </div>
+          <button className="border font-semibold text-s">Apply a Coupon</button>
+          <button className="border font-semibold">Checkout</button>
+          </div>
+          
+        </div>
+      </div>
+      
     </main>
   );
 }
