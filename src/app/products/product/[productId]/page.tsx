42f86788e9716ead5d6d831047718cde343316f7
kdi-0 <kerwin.dahomey@gmail.com>
2023-11-24 16:20:52 -0600

    Implement productDetails page

--- src/app/products/product/[productId]/page.tsx
+++ src/app/products/product/[productId]/page.tsx
@@ -1,7 +1,38 @@
-export default function Page({ params }: { params: { productID: string } }) {
+import Navbar from "@/src/components/Navbar";
+import AddToCart from "@/src/components/AddToCart";
+import prisma from '@/src/app/prismadb';
+export default async function Page({ params }: { params: { productId } }) {
+  const currentId = parseInt(params.productId);
+  const product = await prisma.product.findUnique({
+    where: { PRODUCT_ID: currentId, },
+  });
+  const imageUrl = product.PRODUCT_IMAGE.split('","')[0].slice(2, -2);
   return (
-    <main>
-      <div>Product ID: {params.productID}</div>
-    </main>
+    <div>
+      <Navbar />
+      <div className="container mx-auto my-8 p-5">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-1/2 p-2">
+            <img src={imageUrl} alt={product.PRODUCT_NAME} className="w-full object-contain h-48 sm:h-72" />
+          </div>
+          <div className="w-full sm:w-1/2 p-2">
+            <div className="bg-green-100 p-5">
+              <h2 className="text-2xl font-bold mb-2">{product.PRODUCT_NAME}</h2>
+              <p className="text-xl text-gray-800 mb-4">${product.PRODUCT_PRICE.toFixed(2)}</p>
+              <div className="mb-4">
+                <button className="text-sm bg-gray-200 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300">
+                  1 +
+                </button>
+              </div>
+              <AddToCart />
+            </div>
+            <div className="mt-4 p-5 bg-gray-100 border-t border-gray-200">
+              <h3 className="text-lg font-semibold mb-2">Product Description</h3>
+              <p className="text-gray-600">{product.PRODUCT_DESCRIPTION}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,6 +1,5 @@
 'use client'
 import axios from 'axios';
-import React from 'react';
 import { useSession } from 'next-auth/react';
 
 // MUST SEPARATE client side components such as on click events into their own component (put use client at top of file to specify this) and not be async. Async functions cannot be client side b/c waiting on server to provide response? (RAMIN NOTES)
@@ -11,26 +10,27 @@ const AddToCart = (props) => {
 
     return (
         <div>
-            <button onClick={() => {
-                axios.post('/api/cart/add', {
-                    name: session.user.name,
-                    email: session.user.email,
-                    PRODUCT_ID: product.PRODUCT_ID,
-                    PRODUCT_NAME: product.PRODUCT_NAME,
-                    PRODUCT_QUANTITY: product.PRODUCT_QUANTITY,
-                    PRODUCT_PRICE: product.PRODUCT_PRICE,
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-                    alert("Added item to cart!")
-                }).catch((error) => {
-                    console.log("ERROR");
-                    console.log(error);
-                });
-            }
-            }>Add To Cart</button>
+            <button className="w-full bg-green-500 text-white p-3 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+                onClick={() => {
+                    axios.post('/api/cart/add', {
+                        name: session.user.name,
+                        email: session.user.email,
+                        PRODUCT_ID: product.PRODUCT_ID,
+                        PRODUCT_NAME: product.PRODUCT_NAME,
+                        PRODUCT_QUANTITY: product.PRODUCT_QUANTITY,
+                        PRODUCT_PRICE: product.PRODUCT_PRICE,
+                    }, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }).then((response) => {
+                        alert("Added item to cart!")
+                    }).catch((error) => {
+                        console.log("ERROR");
+                        console.log(error);
+                    });
+                }
+                }>Add To Cart</button>
         </div>
     );
 };
