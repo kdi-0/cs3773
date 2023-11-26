57625d7cb7616bb5b30428ec9d3eb433c9e0db98
kdi-0 <97331704+kdi-0@users.noreply.github.com>
2023-11-25 19:55:27 -0600

    Implement ProductDetails page (#8)
    * Implement productDetails page
    
    * pass product object to cart component
    
    * display quantity on productdetails
    
    * Add invalid product handler and remove placeholder images
    
    * Add support for custom quantity in productdetails page
    
    * Update cart backend to use new cart logic

--- src/app/products/product/[productId]/page.tsx
+++ src/app/products/product/[productId]/page.tsx
@@ -1,7 +1,33 @@
-export default function Page({ params }: { params: { productID: string } }) {
-  return (
-    <main>
-      <div>Product ID: {params.productID}</div>
-    </main>
-  );
+import Navbar from "@/src/components/Navbar";
+import prisma from '@/src/app/prismadb';
+import Link from "next/link";
+import ProductDetails from "@/src/components/ProductDetails";
+export default async function Page({ params }: { params: { productId: string } }) {
+  const currentId = parseInt(params.productId);
+  const product = await prisma.product.findUnique({
+    where: { PRODUCT_ID: currentId, },
+  });
+  if (product === null) {
+    return (
+      <div>
+        <Navbar />
+        <div className="container mx-auto my-8 p-5">
+          <div className="text-center">
+            <h2 className="text-xl font-semibold mb-4">Product Not Found</h2>
+            <p>The product you're looking for does not exist or may have been removed.</p>
+            <Link href="/products" className="text-blue-500 hover:underline">
+              Go back to products
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Navbar />
+        <ProductDetails product={product} />
+      </div>
+    );
+  }
 }
--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,30 +1,32 @@
 'use client'
 import axios from 'axios';
-import React from 'react';
 import { useSession } from 'next-auth/react';
 
 // MUST SEPARATE client side components such as on click events into their own component (put use client at top of file to specify this) and not be async. Async functions cannot be client side b/c waiting on server to provide response? (RAMIN NOTES)
 const AddToCart = (props) => {
 
     const { data: session } = useSession();
-    const { product } = props;
+    let { product, intentQuantity } = props;
 
-    return (
-        <div>
-            <button onClick={() => {
-                axios.post('/api/cart/add', {
-                    name: session.user.name,
-                    email: session.user.email,
-                    PRODUCT_ID: product.PRODUCT_ID,
-                    PRODUCT_NAME: product.PRODUCT_NAME,
-                    PRODUCT_QUANTITY: product.PRODUCT_QUANTITY,
-                    PRODUCT_PRICE: product.PRODUCT_PRICE,
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-                    switch(response.data.status){
+    const imageUrl = product.PRODUCT_IMAGE.split('","')[0].slice(2, -2);
+    const isOutOfStock = product.PRODUCT_QUANTITY === 0;
+    const buttonClasses = `w-full p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-green-300 ${isOutOfStock ? 'bg-gray-500 hover:bg-gray-500 cursor-not-allowed' : 'bg-green-500 hover:bg-green-600'}`;
+    const handleButtonClick = () => {
+        const quantityToAdd = Number.isNaN(intentQuantity) ? 1 : intentQuantity;
+        axios.post('/api/cart/add', {
+            name: session.user.name,
+            email: session.user.email,
+            PRODUCT_ID: product.PRODUCT_ID,
+            PRODUCT_NAME: product.PRODUCT_NAME,
+            quantity: quantityToAdd,
+            PRODUCT_PRICE: product.PRODUCT_PRICE,
+            PRODUCT_IMAGE: imageUrl,
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            switch(response.data.status){
                         case 'success':
                             alert("Added item to cart!");
                             break;
@@ -35,14 +37,21 @@ const AddToCart = (props) => {
                             alert("Cannot add item to cart. You already have the entire available stock for that product in your cart");
                             break;
                     }
-                }).catch((error) => {
-                    console.log("ERROR");
-                    console.log(error);
-                });
-            }
-            }>Add To Cart</button>
+        }).catch((error) => {
+            console.log("ERROR");
+            console.log(error);
+        });
+
+    }
+    return (
+        <div>
+            <button className={buttonClasses}
+                onClick={handleButtonClick}
+              >
+                Add To Cart
+            </button>
         </div>
     );
 };
-
+        
 export default AddToCart;
