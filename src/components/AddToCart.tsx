8185821ec4302a7d5706406436e02015bebab697
kdi-0 <97331704+kdi-0@users.noreply.github.com>
2023-11-28 19:05:55 -0600

    Add proper default quantity value to cart (#9)

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -12,7 +12,7 @@ const AddToCart = (props) => {
     const isOutOfStock = product.PRODUCT_QUANTITY === 0;
     const buttonClasses = `w-full p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-green-300 ${isOutOfStock ? 'bg-gray-500 hover:bg-gray-500 cursor-not-allowed' : 'bg-green-500 hover:bg-green-600'}`;
     const handleButtonClick = () => {
-        const quantityToAdd = Number.isNaN(intentQuantity) ? 1 : intentQuantity;
+        const quantityToAdd = intentQuantity === undefined ? 1 : intentQuantity;
         axios.post('/api/cart/add', {
             name: session.user.name,
             email: session.user.email,
@@ -26,17 +26,17 @@ const AddToCart = (props) => {
                 'Content-Type': 'application/json'
             }
         }).then((response) => {
-            switch(response.data.status){
-                        case 'success':
-                            alert("Added item to cart!");
-                            break;
-                        case 'out of stock':
-                            alert("Product is out of stock. Cannot add product to cart.");
-                            break;
-                        case 'overflow stock':
-                            alert("Cannot add item to cart. You already have the entire available stock for that product in your cart");
-                            break;
-                    }
+            switch (response.data.status) {
+                case 'success':
+                    alert("Added item to cart!");
+                    break;
+                case 'out of stock':
+                    alert("Product is out of stock. Cannot add product to cart.");
+                    break;
+                case 'overflow stock':
+                    alert("Cannot add item to cart. You already have the entire available stock for that product in your cart");
+                    break;
+            }
         }).catch((error) => {
             console.log("ERROR");
             console.log(error);
@@ -47,11 +47,11 @@ const AddToCart = (props) => {
         <div>
             <button className={buttonClasses}
                 onClick={handleButtonClick}
-              >
+            >
                 Add To Cart
             </button>
         </div>
     );
 };
-        
+
 export default AddToCart;
